48f4a73a98c6d98e60c50acf69b600598aeafb70
徐志强 <652732310@qq.com>
2020-04-09 17:55:32 +0800

    add test config

---
+++ pkg/config/config_value_test.go
@@ -0,0 +1,28 @@
+// +build test
+
+package config
+
+import "time"
+
+var config = Value{
+	RestConfig: RestConfig{
+		PublicAddr:      ":2020",
+		IntraAddr:       ":2021",
+		GracefulUpgrade: false,
+		PIDFile:         "/tmp/ont-gateway.pid",
+		ReadTimeout:     time.Second * 3,
+	},
+	LoggerConfig: LoggerConfig{
+		LogLevel: "debug",
+	},
+	RedisCacheConfig: RedisConfig{
+		Addr: "localhost:6379",
+	},
+	MongoConfig: MongoConfig{
+		ConnectionString: "mongodb://localhost:27017/ont",
+		Timeout:          time.Second * 3,
+	},
+	CICDConfig: CICDConfig{
+		AddonDeployAPI: "http://a0d771952588111ea89590659513bb5d-1585432770.ap-southeast-1.elb.amazonaws.com:8000/api/v1/ss",
+	},
+}
