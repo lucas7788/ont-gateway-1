e981b0adcf93683963df61a5fb34dc7889a4890f
徐志强 <652732310@qq.com>
2020-04-15 14:49:27 +0800

    add conf

---
+++ pkg/config/config_value_prod_env.go
@@ -0,0 +1,28 @@
+// +build prod
+
+package config
+
+import "time"
+
+var config = Value{
+	RestConfig: RestConfig{
+		PublicAddr:      ":2020",
+		IntraAddr:       ":2021",
+		GracefulUpgrade: false,
+		PIDFile:         "/tmp/ont-gateway.pid",
+		ReadTimeout:     time.Second * 3,
+	},
+	LoggerConfig: LoggerConfig{
+		LogLevel: "info",
+	},
+	RedisCacheConfig: RedisConfig{
+		Addr: "master.sagamarket-prod.pbcbnm.apne1.cache.amazonaws.com:6379",
+	},
+	MongoConfig: MongoConfig{
+		ConnectionString: "mongodb://sagamarket:S+n7eL1+rSZKtxzC@sagemarket-prod-mongo.cluster-cburg362yfls.ap-northeast-1.docdb.amazonaws.com:27017/ont",
+		Timeout:          time.Second * 3,
+	},
+	CICDConfig: CICDConfig{
+		AddonDeployAPI: "http://a0d771952588111ea89590659513bb5d-1585432770.ap-southeast-1.elb.amazonaws.com:8000/api/v1/ss",
+	},
+}
