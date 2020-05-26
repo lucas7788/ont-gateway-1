38dcf27d67e0948c28d2a87cfdfce692513e62d3
徐志强 <652732310@qq.com>
2020-05-26 20:39:12 +0800

    update MPEndpointGetItemMetaSchemaInput

--- pkg/ddxf/io/mp_endpoint_get_item_meta_schema.go
+++ pkg/ddxf/io/mp_endpoint_get_item_meta_schema.go
@@ -1,9 +1,22 @@
 package io
 
+import io2 "github.com/zhiqiangxu/ont-gateway/pkg/io"
+
+// ItemType ...
+type ItemType int
+
+const (
+	// ItemTypeBook ...
+	ItemTypeBook ItemType = iota
+)
+
 // MPEndpointGetItemMetaSchemaInput ...
 type MPEndpointGetItemMetaSchemaInput struct {
+	ItemType ItemType
 }
 
 // MPEndpointGetItemMetaSchemaOutput ...
 type MPEndpointGetItemMetaSchemaOutput struct {
+	io2.BaseResp
+	ItemMetaSchema map[string]interface{}
 }
