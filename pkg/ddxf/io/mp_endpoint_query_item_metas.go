23836a4aa1bca95d5a0ed263fe3d0ce26bafa13d
徐志强 <652732310@qq.com>
2020-05-27 10:27:35 +0800

    update MPEndpointQueryItemMetas

--- pkg/ddxf/io/mp_endpoint_query_item_metas.go
+++ pkg/ddxf/io/mp_endpoint_query_item_metas.go
@@ -1,9 +1,14 @@
 package io
 
+import io2 "github.com/zhiqiangxu/ont-gateway/pkg/io"
+
 // MPEndpointQueryItemMetasInput ...
 type MPEndpointQueryItemMetasInput struct {
+	Text string
 }
 
 // MPEndpointQueryItemMetasOutput ...
 type MPEndpointQueryItemMetasOutput struct {
+	io2.BaseResp
+	ItemMetas []map[string]interface{}
 }
