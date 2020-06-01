8eb6ce77a7cfed007772a3601aef4371cc97bf30
徐志强 <652732310@qq.com>
2020-06-01 20:59:23 +0800

    update

--- pkg/service/gw_dump_wallet.go
+++ pkg/service/gw_dump_wallet.go
@@ -18,10 +18,10 @@ func (gw *Gateway) DumpWallet(input io.DumpWalletInput) (output io.DumpWalletOut
 	out := time.Now().Format("2006_01_02")
 	cmd := exec.Command("mongodump", "--collection=wallet --out=~/wallet/"+path.Join(input.Path, out)+" --uri="+shellquote.Join(conf.MongoConfig.ConnectionString))
 
-	_, err := cmd.Output()
+	outBytes, err := cmd.Output()
 	if err != nil {
 		output.Code = http.StatusInternalServerError
-		output.Msg = err.Error()
+		output.Msg = string(outBytes)
 		return
 	}
 
