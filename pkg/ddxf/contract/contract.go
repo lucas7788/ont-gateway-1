0a55fd5b360ebf8cd5c38d1783711419d6f58aab
lucas7788 <sishsh@163.com>
2020-06-04 14:15:57 +0800

    fix bug

--- pkg/ddxf/contract/contract.go
+++ pkg/ddxf/contract/contract.go
@@ -5,7 +5,7 @@ import (
 	"github.com/zhiqiangxu/ont-gateway/pkg/ddxf/param"
 )
 
-func ConstructPublishParam(seller common.Address, template param.TokenTemplate,tokenResourceType []*param.TokenResourceTyEndpoint,
+func ConstructPublishParam(seller common.Address, template param.TokenTemplate, tokenResourceType []*param.TokenResourceTyEndpoint,
 	itemMetaHash common.Uint256, fee param.Fee, expiredDate uint64, stock uint32, resourceId string) ([]byte, []byte, []byte) {
 	ddo := param.ResourceDDO{
 		TokenResourceTyEndpoints: tokenResourceType,    // RT for tokens
