e721c23c024c515446971257b5fc5bc7ee377a6d
lucas7788 <sishsh@163.com>
2020-06-09 11:53:50 +0800

    add note

--- pkg/ddxf/example/main.go
+++ pkg/ddxf/example/main.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"encoding/base64"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"github.com/ontio/ontology-go-sdk"
 	"github.com/syndtr/goleveldb/leveldb"
@@ -19,7 +18,6 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
-	"strconv"
 )
 
 var (
@@ -35,7 +33,7 @@ func main() {
 		fmt.Println("bs: ", string(bs))
 		return
 	}
-	if false {
+	if true {
 		res, err := upload()
 		if err != nil {
 			fmt.Println("error: ", err)
@@ -53,15 +51,18 @@ func main() {
 	wallet, _ := ontology_go_sdk.OpenWallet("/Users/sss/gopath/src/github.com/zhiqiangxu/ont-gateway/pkg/ddxf/example/wallet.dat")
 	seller, _ := wallet.GetAccountByAddress("Aejfo7ZX5PVpenRj23yChnyH64nf8T1zbu", pwd)
 	if false {
+		//1.upload
 		fileKey, err := upload()
 		if err != nil {
 			fmt.Println("error: ", err)
 		}
+		//2.SaveDataMeta
 		saveDataMetaOutPut, saveDataMetaInput, err := seller_buyer.SaveDataMeta(fileKey)
 		if err != nil {
 			fmt.Println("error: ", err)
 			return
 		}
+		//3.SaveTokenMeta
 		fmt.Println("DataId: ", saveDataMetaOutPut.DataId)
 		db.Put(key_data_id, []byte(saveDataMetaOutPut.DataId), nil)
 		saveTokenMetaInput, err := seller_buyer.SaveTokenMeta(saveDataMetaInput.DataMetaHash)
@@ -69,6 +70,7 @@ func main() {
 			fmt.Println("error: ", err)
 			return
 		}
+		//4.PublishMeta
 		fmt.Println("TokenMetaHash:", saveTokenMetaInput.TokenMetaHash)
 		db.Put(key_token_meta_hash, []byte(saveTokenMetaInput.TokenMetaHash), nil)
 		resourceId, err := seller_buyer.PublishMeta(seller, saveDataMetaOutPut, saveDataMetaInput, saveTokenMetaInput)
@@ -85,11 +87,13 @@ func main() {
 
 	buyer, _ := wallet.GetAccountByAddress("AHhXa11suUgVLX1ZDFErqBd3gskKqLfa5N", pwd)
 	if true {
+		//5.BuyDtoken
 		err := seller_buyer.BuyDtoken(buyer, string(resourceId))
 		if err != nil {
 			fmt.Println("error:", err)
 			return
 		}
+		//6.UseToken
 		err = seller_buyer.UseToken(buyer, string(resourceId), string(tokenMetaHash), string(dataId))
 		if err != nil {
 			panic(err)
@@ -124,105 +128,6 @@ func download2() {
 	fmt.Println("data: ", string(data))
 }
 
-func download() error {
-	url := "http://127.0.0.1:20335/ddxf/storage/upload"
-	return downloadFile(url, "./download", func(length, downLen int64) {
-		fmt.Printf("length: %d, downLen: %d\n", length, downLen)
-	})
-}
-func isFileExist(filename string, filesize int64) bool {
-	info, err := os.Stat(filename)
-	if os.IsNotExist(err) {
-		fmt.Println(info)
-		return false
-	}
-	if filesize == info.Size() {
-		fmt.Println("安装包已存在！", info.Name(), info.Size(), info.ModTime())
-		return true
-	}
-	del := os.Remove(filename)
-	if del != nil {
-		fmt.Println(del)
-	}
-	return false
-}
-
-func downloadFile(url string, localPath string, fb func(length, downLen int64)) error {
-	var (
-		fsize   int64
-		buf     = make([]byte, 32*1024)
-		written int64
-	)
-	tmpFilePath := localPath + ".download"
-	fmt.Println(tmpFilePath)
-	//创建一个http client
-	client := new(http.Client)
-	//client.Timeout = time.Second * 60 //设置超时时间
-	//get方法获取资源
-	resp, err := client.Get(url)
-	if err != nil {
-		return err
-	}
-
-	//读取服务器返回的文件大小
-	fsize, err = strconv.ParseInt(resp.Header.Get("Content-Length"), 10, 32)
-	if err != nil {
-		fmt.Println(err)
-	}
-	if isFileExist(localPath, fsize) {
-		return err
-	}
-	fmt.Println("fsize", fsize)
-	//创建文件
-	file, err := os.Create(tmpFilePath)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-	if resp.Body == nil {
-		return errors.New("body is null")
-	}
-	defer resp.Body.Close()
-	//下面是 io.copyBuffer() 的简化版本
-	for {
-		//读取bytes
-		nr, er := resp.Body.Read(buf)
-		if nr > 0 {
-			//写入bytes
-			nw, ew := file.Write(buf[0:nr])
-			//数据长度大于0
-			if nw > 0 {
-				written += int64(nw)
-			}
-			//写入出错
-			if ew != nil {
-				err = ew
-				break
-			}
-			//读取是数据长度不等于写入的数据长度
-			if nr != nw {
-				err = io.ErrShortWrite
-				break
-			}
-		}
-		if er != nil {
-			if er != io.EOF {
-				err = er
-			}
-			break
-		}
-		//没有错误了快使用 callback
-		fb(fsize, written)
-	}
-	fmt.Println(err)
-	if err == nil {
-		file.Close()
-		err = os.Rename(tmpFilePath, localPath)
-		fmt.Println(err)
-	}
-	return err
-}
-
 func upload() (string, error) {
 	url := "http://127.0.0.1:20335/ddxf/storage/upload"
 	path := "./pkg/ddxf/example/wallet.dat"
@@ -251,7 +156,7 @@ func upload() (string, error) {
 	rr := make(map[string]interface{})
 	err = json.Unmarshal(body.Bytes(), rr)
 	if err != nil {
-		return "", nil
+		return "", err
 	}
 	return rr["fileName"].(string), nil
 }
