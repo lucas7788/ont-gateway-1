400d8dfbede017a7eee2c9dbd8528ffb1c3bb57d
徐志强 <652732310@qq.com>
2020-06-15 17:50:20 +0800

    update

--- app/manage/cmd/print.go
+++ app/manage/cmd/print.go
@@ -6,10 +6,15 @@ import (
 
 	"github.com/urfave/cli/v2"
 	"github.com/zhiqiangxu/ont-gateway/pkg/io"
+	"github.com/zhiqiangxu/ont-gateway/pkg/model"
 )
 
 // Print ...
 func Print(c *cli.Context) (err error) {
+	ak, _ := model.AppManager().GenerateAkSk()
+	fmt.Println(ak)
+	return
+
 	bytes, err := json.Marshal(io.GetResourceOutput{})
 	if err != nil {
 		return
