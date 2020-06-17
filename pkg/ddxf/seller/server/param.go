d1703d99dd4ecbc799ee57d8f761b43ad034e157
lucas7788 <sishsh@163.com>
2020-06-17 15:21:56 +0800

    add freeze

--- pkg/ddxf/seller/server/param.go
+++ pkg/ddxf/seller/server/param.go
@@ -1,6 +1,25 @@
 package server
 
+import (
+	"github.com/zhiqiangxu/ont-gateway/pkg/ddxf/io"
+	io2 "github.com/zhiqiangxu/ont-gateway/pkg/io"
+)
+
 type ItemMeta struct {
 	ItemMetaHash string                 `bson:"itemMetaHash" json:"itemMetaHash"`
 	ItemMetaData map[string]interface{} `bson:"itemMetaData" json:"itemMetaData"`
+}
+
+type PublishForOpenKgParam struct {
+	SellerSaveDataMetaInput        io.SellerSaveDataMetaInput
+	SellerSaveTokenMetaInput       io.SellerSaveTokenMetaInput
+	MPEndpointPublishItemMetaInput io.MPEndpointPublishItemMetaInput
+}
+
+type OpenKgRes struct {
+	io2.BaseResp
+}
+
+type FreezeParam struct {
+	SignedTx string
 }
