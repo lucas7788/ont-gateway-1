656c8312dcd8644f031b1aa3fe31b8b118eca2a5
徐志强 <652732310@qq.com>
2020-06-18 15:37:42 +0800

    add test

--- pkg/esign/docusign/docusign_test.go
+++ pkg/esign/docusign/docusign_test.go
@@ -13,104 +13,213 @@ import (
 )
 
 func TestDocusign(t *testing.T) {
-	env := &model.EnvelopeDefinition{
-		EmailSubject: "[Go eSignagure SDK] - Please sign this doc",
-		EmailBlurb:   "Please sign this test document",
-		Status:       "sent",
-		Documents: []model.Document{
-			{
-				Name:       "invite letter.pdf",
-				DocumentID: "1",
-			},
-			{
-				Name:       "contract.pdf",
-				DocumentID: "2",
-			},
-		},
-		Recipients: &model.Recipients{
-			Signers: []model.Signer{
+	// remote
+	if false {
+		env := &model.EnvelopeDefinition{
+			EmailSubject: "[Go eSignagure SDK] - Please sign this doc",
+			EmailBlurb:   "Please sign this test document",
+			Status:       "sent",
+			Documents: []model.Document{
 				{
-					Email:             "652732310@qq.com",
-					EmailNotification: nil,
-					Name:              "J F Cote",
-					RecipientID:       "1",
-					Tabs: &model.Tabs{
-						SignHereTabs: []model.SignHere{
-							{
-								TabBase: model.TabBase{
-									DocumentID:  "1",
-									RecipientID: "1",
-								},
-								TabPosition: model.TabPosition{
-									PageNumber: "1",
-									TabLabel:   "signature",
-									XPosition:  "192",
-									YPosition:  "160",
-								},
-							},
-						},
-						DateSignedTabs: []model.DateSigned{
-							{
-								TabBase: model.TabBase{
-									DocumentID:  "1",
-									RecipientID: "1",
-								},
-								TabPosition: model.TabPosition{
-									PageNumber: "1",
-									TabLabel:   "dateSigned",
-									XPosition:  "334",
-									YPosition:  "179",
+					Name:       "invite letter.pdf",
+					DocumentID: "1",
+				},
+				{
+					Name:       "contract.pdf",
+					DocumentID: "2",
+				},
+			},
+			Recipients: &model.Recipients{
+				Signers: []model.Signer{
+					{
+						Email:             "652732310@qq.com",
+						EmailNotification: nil,
+						Name:              "J F Cote",
+						RecipientID:       "1",
+						Tabs: &model.Tabs{
+							SignHereTabs: []model.SignHere{
+								{
+									TabBase: model.TabBase{
+										DocumentID:  "1",
+										RecipientID: "1",
+									},
+									TabPosition: model.TabPosition{
+										PageNumber: "1",
+										TabLabel:   "signature",
+										XPosition:  "192",
+										YPosition:  "160",
+									},
 								},
 							},
-						},
-						TextTabs: []model.Text{
-							{
-								TabBase: model.TabBase{
-									DocumentID:  "2",
-									RecipientID: "1",
+							DateSignedTabs: []model.DateSigned{
+								{
+									TabBase: model.TabBase{
+										DocumentID:  "1",
+										RecipientID: "1",
+									},
+									TabPosition: model.TabPosition{
+										PageNumber: "1",
+										TabLabel:   "dateSigned",
+										XPosition:  "334",
+										YPosition:  "179",
+									},
 								},
-								TabPosition: model.TabPosition{
-									PageNumber: "1",
-									TabLabel:   "txtNote",
-									XPosition:  "70",
-									YPosition:  "564",
+							},
+							TextTabs: []model.Text{
+								{
+									TabBase: model.TabBase{
+										DocumentID:  "2",
+										RecipientID: "1",
+									},
+									TabPosition: model.TabPosition{
+										PageNumber: "1",
+										TabLabel:   "txtNote",
+										XPosition:  "70",
+										YPosition:  "564",
+									},
+									TabStyle: model.TabStyle{
+										Name: "This is the tab tooltip",
+									},
+									Width:  "300",
+									Height: "150",
 								},
-								TabStyle: model.TabStyle{
-									Name: "This is the tab tooltip",
-								},
-								Width:  "300",
-								Height: "150",
 							},
 						},
 					},
 				},
 			},
-		},
+		}
+
+		// open files for upload
+		f1, err := os.Open("letter.pdf")
+		assert.Nil(t, err)
+		f2, err := os.Open("contract.pdf")
+		assert.Nil(t, err)
+
+		url := docu.UserConsentURL()
+		assert.True(t, url != "")
+
+		ioutil.WriteFile("/tmp/consent.txt", []byte(url), 0644)
+
+		_, err = docu.SendEnvelope(context.TODO(), env, &esign.UploadFile{
+			ContentType: "application/pdf",
+			FileName:    "invitation letter.pdf",
+			ID:          "1",
+			Reader:      f1,
+		}, &esign.UploadFile{
+			ContentType: "application/pdf",
+			FileName:    "contract.pdf",
+			ID:          "2",
+			Reader:      f2,
+		})
+		assert.Nil(t, err)
 	}
 
-	// open files for upload
-	f1, err := os.Open("letter.pdf")
-	assert.Nil(t, err)
-	f2, err := os.Open("contract.pdf")
-	assert.Nil(t, err)
+	// embeded
+	if true {
+		env := &model.EnvelopeDefinition{
+			EmailSubject: "[Go eSignagure SDK] - Please sign this doc(embeded)",
+			EmailBlurb:   "Please sign this test document",
+			Status:       "sent",
+			Documents: []model.Document{
+				{
+					Name:       "invite letter.pdf",
+					DocumentID: "1",
+				},
+				{
+					Name:       "contract.pdf",
+					DocumentID: "2",
+				},
+			},
+			Recipients: &model.Recipients{
+				Signers: []model.Signer{
+					{
+						Email:             "652732310@qq.com",
+						EmailNotification: nil,
+						Name:              "J F Cote",
+						RecipientID:       "1",
+						ClientUserID:      "123",
+						Tabs: &model.Tabs{
+							SignHereTabs: []model.SignHere{
+								{
+									TabBase: model.TabBase{
+										DocumentID:  "1",
+										RecipientID: "1",
+									},
+									TabPosition: model.TabPosition{
+										PageNumber: "1",
+										TabLabel:   "signature",
+										XPosition:  "192",
+										YPosition:  "160",
+									},
+								},
+							},
+							DateSignedTabs: []model.DateSigned{
+								{
+									TabBase: model.TabBase{
+										DocumentID:  "1",
+										RecipientID: "1",
+									},
+									TabPosition: model.TabPosition{
+										PageNumber: "1",
+										TabLabel:   "dateSigned",
+										XPosition:  "334",
+										YPosition:  "179",
+									},
+								},
+							},
+							TextTabs: []model.Text{
+								{
+									TabBase: model.TabBase{
+										DocumentID:  "2",
+										RecipientID: "1",
+									},
+									TabPosition: model.TabPosition{
+										PageNumber: "1",
+										TabLabel:   "txtNote",
+										XPosition:  "70",
+										YPosition:  "564",
+									},
+									TabStyle: model.TabStyle{
+										Name: "This is the tab tooltip",
+									},
+									Width:  "300",
+									Height: "150",
+								},
+							},
+						},
+					},
+				},
+			},
+		}
 
-	url := docu.UserConsentURL()
-	assert.True(t, url != "")
+		// open files for upload
+		f1, err := os.Open("letter.pdf")
+		assert.Nil(t, err)
+		f2, err := os.Open("contract.pdf")
+		assert.Nil(t, err)
 
-	ioutil.WriteFile("/tmp/consent.txt", []byte(url), 0644)
+		url := docu.UserConsentURL()
+		assert.True(t, url != "")
 
-	_, err = docu.SendEnvelope(context.TODO(), env, &esign.UploadFile{
-		ContentType: "application/pdf",
-		FileName:    "invitation letter.pdf",
-		ID:          "1",
-		Reader:      f1,
-	}, &esign.UploadFile{
-		ContentType: "application/pdf",
-		FileName:    "contract.pdf",
-		ID:          "2",
-		Reader:      f2,
-	})
-	assert.Nil(t, err)
+		ioutil.WriteFile("/tmp/consent.txt", []byte(url), 0644)
+
+		summary, err := docu.SendEnvelope(context.TODO(), env, &esign.UploadFile{
+			ContentType: "application/pdf",
+			FileName:    "invitation letter.pdf",
+			ID:          "1",
+			Reader:      f1,
+		}, &esign.UploadFile{
+			ContentType: "application/pdf",
+			FileName:    "contract.pdf",
+			ID:          "2",
+			Reader:      f2,
+		})
+		assert.True(t, err == nil && summary.EnvelopeID != "")
+
+		// follow  https://developers.docusign.com/esign-rest-api/guides/concepts/embedding#step-2-generate-the-recipient-signing-url
+
+	}
 }
 
 var (
