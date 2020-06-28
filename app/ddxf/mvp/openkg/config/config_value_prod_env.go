51c191ed15cb50023de7d2dd12d8b22a7a2d2339
徐志强 <652732310@qq.com>
2020-06-28 11:31:13 +0800

    mod prod conf for ddxf

--- app/ddxf/mvp/openkg/config/config_value_prod_env.go
+++ app/ddxf/mvp/openkg/config/config_value_prod_env.go
@@ -5,4 +5,6 @@ package config
 const (
 	// OpenKGCallbackURI ...
 	OpenKGCallbackURI = ""
+	SellerUrl         = "http://ddxf-component-seller-service:20332"
+	MPEndpoint        = "http://ddxf-component-mp-service:20333"
 )
--- pkg/ddxf/config/config_value_prod_env.go
+++ pkg/ddxf/config/config_value_prod_env.go
@@ -1,3 +1,11 @@
 // +build prod
 
 package config
+
+const (
+	OperatorPrivateKey = ""
+
+	MpUrl     = "http://ddxf-component-mp-service:" + MpPort
+	SellerUrl = "http://ddxf-component-seller-service:" + SellerPort
+	BuyerUrl  = "http://ddxf-component-buyer-service:" + BuyerPort
+)
