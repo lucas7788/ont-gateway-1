c14ff17e8d933349a1ffd0380a5fa391b59e70b9
lucas7788 <sishsh@163.com>
2020-06-30 16:27:04 +0800

    add regIdArray

--- app/ddxf/mvp/openkg/server/utils.go
+++ app/ddxf/mvp/openkg/server/utils.go
@@ -20,6 +20,57 @@ import (
 	"net/http"
 )
 
+type Signer struct {
+	Id    []byte
+	Index uint32
+}
+func (this *Signer) Serialize(sink *common.ZeroCopySink) {
+	sink.WriteVarBytes(this.Id)
+	sink.WriteUint32(this.Index)
+}
+type Group struct {
+	Members   [][]byte
+	Threshold uint
+}
+func (this *Group) Serialize(sink *common.ZeroCopySink) {
+	sink.WriteVarUint(uint64(len(this.Members)))
+	for _,item := range this.Members {
+		sink.WriteVarBytes(item)
+	}
+	sink.WriteUint32(uint32(this.Threshold))
+}
+type RegIdParam struct {
+	Ontid []byte
+	Group Group
+	Signer []Signer
+	Attributes []DDOAttribute
+}
+
+func (this *RegIdParam) Serialize(sink *common.ZeroCopySink) {
+	sink.WriteVarBytes(this.Ontid)
+	this.Group.Serialize(sink)
+	sink.WriteVarUint(uint64(len(this.Signer)))
+	for _,signer := range this.Signer {
+		signer.Serialize(sink)
+	}
+	sink.WriteVarUint(uint64(len(this.Attributes)))
+	for _, attr := range this.Attributes {
+		attr.Serialize(sink)
+	}
+}
+
+type DDOAttribute struct {
+	Key       []byte
+	Value     []byte
+	ValueType []byte
+}
+func (this *DDOAttribute) Serialize(sink *common.ZeroCopySink) {
+	sink.WriteVarBytes(this.Key)
+	sink.WriteVarBytes(this.Value)
+	sink.WriteVarBytes(this.ValueType)
+}
+
+
 func GetAccount(userId string) *ontology_go_sdk.Account {
 	plainSeed := []byte(defPlainSeed + userId)
 	pri, pubKey := key_manager.GetKeyPair(plainSeed)
@@ -156,6 +207,7 @@ func deletePublish(resourceId string, seller *ontology_go_sdk.Account, headers m
 		return
 	}
 	err = res.Error()
+	return
 }
 
 func queryDataIdFromSeller(dataMetas []map[string]interface{}) (map[string]interface{}, [][sha256.Size]byte, error) {
